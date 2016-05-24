4e66f8b0721cf3dfc177b83cb07062a1dd4d2fe2
Eunji Shim <eunji991222@gmail.com>
2016-05-24 20:09:33 +0900

    #1 edit layout

--- app/src/main/java/kr/hs/emirim/kokopink1/viewflippertest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kokopink1/viewflippertest/MainActivity.java
@@ -2,12 +2,33 @@ package kr.hs.emirim.kokopink1.viewflippertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper viewFlip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butPrev=(Button)findViewById(R.id.but_prev);
+        Button butNext=(Button)findViewById(R.id.but_next);
+        butPrev.setOnClickListener(this);
+        butNext.setOnClickListener(this);
+        viewFlip=(ViewFlipper)findViewById(R.id.view_flip);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.but_prev:
+                viewFlip.showPrevious(); //이전화면
+                break;
+            case R.id.but_next:
+                viewFlip.showNext(); //다음화면
+                break;
+        }
+
     }
 }
